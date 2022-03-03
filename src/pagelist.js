0f6191a067778a070f572b98752ef7ab64c8388c
kimtahen <kimtahen@naver.com>
2022-03-03 15:47:09 +0900

    pagelist example added

---
+++ src/pagelist.js
@@ -0,0 +1,93 @@
+let pagelist = [
+  {
+    _id: new ObjectId("62202bcd0c19efdaa2dd719e"),
+    title: 'category-test12',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:33.834Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  },
+  {
+    _id: new ObjectId("62202bc90c19efdaa2dd719b"),
+    title: 'category-test11',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:29.673Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  },
+  {
+    _id: new ObjectId("62202bc40c19efdaa2dd7198"),
+    title: 'category-test10',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:24.932Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  },
+  {
+    _id: new ObjectId("62202bc00c19efdaa2dd7195"),
+    title: 'category-test9',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:20.986Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  },
+  {
+    _id: new ObjectId("62202bbe0c19efdaa2dd7192"),
+    title: 'category-test8',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:18.060Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  },
+  {
+    _id: new ObjectId("62202bba0c19efdaa2dd718f"),
+    title: 'category-test7',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:14.802Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  },
+  {
+    _id: new ObjectId("62202bb70c19efdaa2dd718c"),
+    title: 'category-test6',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:11.593Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  },
+  {
+    _id: new ObjectId("62202bb30c19efdaa2dd7189"),
+    title: 'category-test5',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:07.941Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  },
+  {
+    _id: new ObjectId("62202bb00c19efdaa2dd7186"),
+    title: 'category-test4',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:04.625Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  },
+  {
+    _id: new ObjectId("62202bad0c19efdaa2dd7183"),
+    title: 'category-test3',
+    author: '김태현',
+    uploadDate: 2022-03-03T02:45:01.715Z,
+    category: 'javascript',
+    data: '<p>aefaff</p>\n',
+    __v: 0
+  }
+]
+export default pagelist;
