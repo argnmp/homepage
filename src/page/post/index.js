d9643736d4a3beea6771900c61d36f0cf20c7f3a
kimtahen <kimtahen@naver.com>
2022-03-10 16:46:25 +0900

    make options invisible when the user is not logined

--- src/page/post/index.js
+++ src/page/post/index.js
@@ -11,6 +11,7 @@ export const Post = () => {
     const pageData = useSelector(state => state.page.currentPageData);
     const pageMetadata = useSelector(state=>state.page.currentPageMetadata);
     let common = useSelector(state=>state.common);
+    const user = useSelector(state => state.user);
 
     const deleteHandler = (e)=>{
         e.preventDefault();
@@ -32,10 +33,12 @@ export const Post = () => {
                         <span>{moment(pageMetadata.uploadDate).format('YYYY-MM-DD hh:mm:ss')}  </span>
                         <span><i>posted by </i>{pageMetadata.author}</span>
                     </div>
+                    {user.isLogined && 
                     <div className="options">
                         <span><a href={`/upload/${pageMetadata.uri}`}>수정</a></span>
                         <span onClick={(e)=>{deleteHandler(e)}}>삭제</span>
                     </div>
+                    }
                 </div>
                 <div className="wrapper">
                 <div className="markdown-body" dangerouslySetInnerHTML={{__html:pageData}}></div>
