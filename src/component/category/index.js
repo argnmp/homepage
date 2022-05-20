9b05addb8bc09f14efaed3f5b7b8d98ca02b3899
kimtahen <kimtahen@naver.com>
2022-05-20 21:32:50 +0900

    slight design change

--- src/component/category/index.js
+++ src/component/category/index.js
@@ -25,12 +25,21 @@ export const Category = () => {
         for(let i in list){
             if(typeof(list[i])=='object'){        
                 const targetUrl = getUrl(i,true,upperPage);
-                returnElement.push(<li key={i} onClick={(e)=>{e.stopPropagation(); document.location.href=targetUrl;}}><span>{i}</span>{createCategory(list[i], false, i)}</li>)        
+                returnElement.push(<li key={i} >
+                    <div className='category-text'>
+                        <span onClick={(e)=>{e.stopPropagation(); document.location.href=targetUrl;}}>{i}</span>
+                    </div>
+                    {createCategory(list[i], false, i)}
+                    </li>)        
                 //stopPropagation이 없다면 onclick 이벤트가 버블링 되어서 상위의 카테고리가 마지막으로 눌리게 됨. 이것때문에 많이 고생함..
             }else{
 
                 const targetUrl = getUrl(i,list[i],upperPage);
-                returnElement.push(<li key={i} onClick={(e)=>{e.stopPropagation(); document.location.href=targetUrl;}}><span>{i}</span></li>);
+                returnElement.push(<li key={i}>
+                    <div className='category-text'>
+                        <span onClick={(e)=>{e.stopPropagation(); document.location.href=targetUrl;}}>{i}</span>
+                    </div>
+                    </li>);
             }
         }
         if(isUpper){
