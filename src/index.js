ec7b26d2c330823d53e3c08637be35861c3c257b
kimtahen <kimtahen@naver.com>
2022-07-18 16:39:38 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import {Provider} from 'react-redux';
 import rootReducer, {rootSaga} from './modules';
 import createSagaMiddleware from 'redux-saga';
 
+const preloadedState = window.__PRELOADED_STATE__;
+delete window.__PRELOADED_STATE__;
+
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, preloadedState, compose(applyMiddleware(sagaMiddleware)));
 sagaMiddleware.run(rootSaga);
